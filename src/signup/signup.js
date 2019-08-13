5de19ae607e747bdf5de218922dfb857529c0e0c
hugginssd <hugginsmafigu@gmail.com>
2019-08-13 16:09:23 +0200

    signup component

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -8,6 +8,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import styles from './style';
+import { FormControlLabel } from '@material-ui/core';
+import { Create } from '@material-ui/icons'
 
 class SignupComponent extends React.Component {
 
@@ -16,20 +18,60 @@ class SignupComponent extends React.Component {
         const { classes } = this.props;
 
 
-
         return ( <
             main className = { classes.main } >
             <
-            CssBaseline >
-
-            <
-            /CssBaseline> <
+            CssBaseline > < /CssBaseline> <
             Paper className = { classes.paper } >
-
             <
+            Typography component = 'h1'
+            variant = 'h5' >
+            Sign Up!
+            <
+            /Typography> <
+            form onSubmit = {
+                (e) => this.submitSignup(e) }
+            className = { classes.form } >
+            <
+            FormControl required fullWidth margin = 'normal' >
+            <
+            InputLabel htmlFor = 'signup-email-input' > Enter your email < /InputLabel> <
+            Input autoComplete = 'email'
+            onChange = {
+                (e) => this.userTyping('email', e) }
+            autoFocus id = 'signup-email-input' > < /Input> <
+            /FormControl> <
+            FormControl required fullWidth margin = 'normal' >
+            <
+            InputLabel htmlFor = 'signup-password-input' > Create a password < /InputLabel> <
+            Input type = 'password'
+            onChange = {
+                (e) => this.userTyping('password', e) }
+            id = 'signup-password-input' > < /Input> <
+            /FormControl> <
+            FormControl required fullWidth margin = 'normal' >
+            <
+            InputLabel htmlFor = 'signup-password-confirmation-input' > Confirm your password < /InputLabel> <
+            Input type = 'password'
+            onChange = {
+                (e) => this.userTyping('passwordConfirmation', e) }
+            id = 'signup-password-confirmation-input' > < /Input> <
+            /FormControl> <
+            Button type = 'submit'
+            fullWidth variant = 'contained'
+            color = 'primary'
+            className = { classes.submit } > Submit < /Button> <
+            /form> <
             /Paper> <
             /main>
         )
+    }
+
+    userTyping = (type, e) => {
+        console.log(type, e);
+    }
+    submitSignup = (e) => {
+        console.log('Submitting');
     }
 
 }
