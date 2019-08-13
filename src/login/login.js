0d5abd6397e72bb8075af429f6d16d0a0851c6d8
hugginssd <hugginsmafigu@gmail.com>
2019-08-13 14:55:28 +0200

    Set components login, signup, dashboard"

---
+++ src/login/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LoginComponent extends React.Component {
+
+        render() {
+            return ( < div > Hello world from Login < /div>)
+            }
+
+        }
+
+        export default LoginComponent;
---
+++ src/signup/signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SignupComponent extends React.Component {
+
+        render() {
+            return ( < div > Hello world from SignUp < /div>)
+            }
+
+        }
+
+        export default SignupComponent;
