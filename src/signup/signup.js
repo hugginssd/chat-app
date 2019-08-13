6bb7857ae8741d999d8bb1a922bc36f76941a78e
hugginssd <hugginsmafigu@gmail.com>
2019-08-13 15:20:16 +0200

    styles.css in signup component

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import styles from './style';
 
 class SignupComponent extends React.Component {
 
@@ -8,4 +17,4 @@ class SignupComponent extends React.Component {
 
         }
 
-        export default SignupComponent;
+        export default withStyles(styles)(SignupComponent);
